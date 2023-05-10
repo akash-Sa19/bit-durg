cfeba51d8aac57280b624bf1b20d7f149d41c29a
Akash Sahu <akashsahu793@gmail.com>
2023-05-10 12:55:37 +0530

    Akash - Changed Title and TitleIcon and asset.js
    In asset.js there was a Duplicate export that i fixed.

--- src/assets/asset.js
+++ src/assets/asset.js
@@ -7,20 +7,10 @@ import undergraduate from './undergraduate.svg'
 import scholarship from './scholarship.png'
 import campusLife from './campusLife.png'
 import whoWeAre from './whoWeAre.png'
-import calender from './icons_calender.svg';
 import event_icon from './event_icon.svg'
 import news_icon from './news_icon.svg'
 import filter from './filter.svg'
 import img1 from './img1.png'
-import search from './search.svg';
-import Grouplogo1 from './Grouplogo1.svg';
-import downArrow from './downArrow.svg'
-import hero from './hero.png'
-import graduate from './graduate.svg'
-import undergraduate from './undergraduate.svg'
-import scholarship from './scholarship.png'
-import campusLife from './campusLife.png'
-import whoWeAre from './whoWeAre.png'
 import calender from './icons_calender.svg'
 import startDoubleQuote from './startDoubleQuote.svg'
 import endDoubleQuote from './endDoubleQuote.svg'
@@ -35,7 +25,11 @@ export {
 	search,
 	Grouplogo1,
 	downArrow,
-	hero, event_icon, news_icon, filter, img1,
+	hero, 
+	event_icon, 
+	news_icon, 
+	filter, 
+	img1,
 	graduate, 
 	undergraduate, 
 	scholarship, 
