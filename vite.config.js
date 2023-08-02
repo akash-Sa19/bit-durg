0b111f233c83ebc784fba1dd90546e6f06837a93
Akash Sahu <akashsahu793@gmail.com>
2023-08-02 19:48:52 +0530

    Akash - gh-pages works

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: 'build'
+  },
+  base: "/bit-durg",
   plugins: [react()],
 })
